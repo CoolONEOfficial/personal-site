8d43b7ca97e248263a7bbfd885a630027a5bcaa6
coolone <coolone.official@gmail.com>
2019-12-29 16:42:55 +0300

    Deleted dead code from firebase functions and package json

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -54,33 +54,6 @@ for (const key in config.envs) {
   process.env[key.toUpperCase()] = config.envs[key];
 }
 
-// SSR
-
-const { Nuxt } = require("nuxt");
-
-const ssrapp = express();
-const nuxt = new Nuxt({
-  dev: false,
-  debug: true,
-  buildDir: "nuxt",
-  build: {
-    publicPath: "/assets/"
-  }
-});
-
-function handleRequest(req, res) {
-  res.set("Cache-Control", "public, max-age=10, s-maxage=10");
-  return new Promise((resolve, reject) => {
-    nuxt.render(req, res, promise => {
-      promise.then(resolve).catch(reject);
-    });
-  });
-}
-
-ssrapp.use(handleRequest);
-
-exports.ssr = functions.https.onRequest(ssrapp);
-
 // Web api
 
 const main = express();
